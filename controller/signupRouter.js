7ae0e5658e1f10e0fb146e225e1ff41b067e4f15
Nithindas004 <nithindas232001@gmail.com>
2024-02-09 11:29:15 +0530

    password encrypted

--- controller/signupRouter.js
+++ controller/signupRouter.js
@@ -1,15 +1,31 @@
 const express=require("express")
 const signupModel=require("../models/userModel")
+const bcrypt = require("bcryptjs")
 
 const router= express.Router()
 
+hashPasswordGenerator= async(pass)=>{
+    const salt = await bcrypt.genSalt(10)
+    return bcrypt.hash(pass,salt)
+}
+
 router.post("/signup",async(req,res)=>{
-    let data = req.body
-    let signup=new signupModel(data)
-    let result = await signup.save()
-    res.json({
-        status:"success"
-    })
+    let {data} = {"data":req.body}
+    let password= data.password
+    hashPasswordGenerator(password).then(
+        (hashedPassword)=>{
+            console.log(hashedPassword)
+            data.password=hashedPassword
+            let signup=new signupModel(data)
+            let result = signup.save()
+            res.json({
+                status:"success"
+            })
+        }
+    )
+    
+
+    
 })
 
 module.exports=router
