4b24c45fdb86e848d3a5cee9fafb29d5b08a2e8a
Nithindas004 <nithindas232001@gmail.com>
2024-02-09 14:49:14 +0530

    addpost completed

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express= require("express")
 const cors = require("cors")
 const mongoose = require("mongoose")
 const signupRoute = require("./controller/signupRouter")
+const postRoute = require("./controller/postRouter")
 
 const app = express()
 
@@ -11,6 +12,8 @@ app.use(cors())
 mongoose.connect("mongodb+srv://nithindas1234:1234nith@cluster0.lvn9hia.mongodb.net/blogappDb?retryWrites=true&w=majority",
 {useNewUrlParser: true})
 
+
+app.use("/api/post",postRoute)
 app.use("/api/blog",signupRoute)
 
 app.listen(3001,()=>{
